ab40f5d3e97f9e0446a139382e4d87573819a813
qhvuong <76862359+qhvuong@users.noreply.github.com>
2021-08-11 12:11:52 -0400

    Add files via upload

--- DoTemplateFit.C
+++ DoTemplateFit.C
@@ -5,10 +5,12 @@ TH1D * templates[10];
 // The template for the intrinsic nue
 TH1D * intrinsic; 
 // define the energy bins used in the template
-double energy_range[11] = {0., 0.5, 1., 1.5, 2., 2.5, 3., 3.5, 4., 4.5, 5.};
 
 // this is the thing you are trying to fit to, i.e. the data distribution
 TH1D * target;
+
+
+
 
 double getOscProb( double energy, double theta, double dm2 )
 {
@@ -22,12 +24,21 @@ double getOscProb( double energy, double theta, double dm2 )
 double getChi2( const double * par )
 {
 
+  double energy_max = 10;
+  double energy_min = 0;
+  int N = 10;
+  double size = (energy_max-energy_min)/N;
+  double energy_range[N];
+  for(int i = 0; i < N; i++){
+    energy_range[i] = i*size;
+  }
+
   // Create a histogram "temp" from the templates
   // Start with the intrinsic nu_e CC template, which doesn't change with oscillations
   TH1D * temp = (TH1D*)intrinsic->Clone("tmp");
 
   // Add in oscillated neutrinos by taking the nu_mu CC templates and weighting by the oscillation probability
-  for( int i = 0; i < 10; ++i ) {
+  for( int i = 0; i < N; ++i ) {
     double e = (energy_range[i+1] - energy_range[i])/2.;
     double oscProb = getOscProb(e, par[0], par[1]); // par[0] = theta, par[1] = dm2
     temp->Add( templates[i], oscProb );
@@ -48,6 +59,8 @@ double getChi2( const double * par )
 
 }
 
+
+
 bool doFit( double &theta, double &dm2 )
 {
   // Make a Minuit fitter object
@@ -62,7 +75,7 @@ bool doFit( double &theta, double &dm2 )
   fitter->SetVariable( 1, "dm2", 5.0, 0.01 );
 
   // 2 free parameters = theta, dm2
-  ROOT::Math::Functor functor( getChi2, 2 );
+  ROOT::Math::Functor functor( &getChi2, 2 );
   fitter->SetFunction( functor );
 
   // Go!
@@ -81,15 +94,36 @@ bool doFit( double &theta, double &dm2 )
 }
 
 
-
 // main function
 void DoTemplateFit()
 {
-  TFile *f = new TFile("output.root");
+  TFile *f = new TFile("output.root","READ");
+  TH2D* h = (TH2D*)f->Get("hElepVsEv");
+  //for(int i=0; i<10; i++){
+  templates[1] = (TH1D*)h->ProjectionY("bin1",1,1);
+  templates[2] = (TH1D*)h->ProjectionY("bin2",2,2);
+  templates[3] = (TH1D*)h->ProjectionY("bin3",3,3);
+  templates[4] = (TH1D*)h->ProjectionY("bin4",4,4);
+  templates[5] = (TH1D*)h->ProjectionY("bin5",5,5);
+  templates[6] = (TH1D*)h->ProjectionY("bin6",6,6);
+  templates[7] = (TH1D*)h->ProjectionY("bin7",7,7);
+  templates[8] = (TH1D*)h->ProjectionY("bin8",8,8);
+  templates[9] = (TH1D*)h->ProjectionY("bin9",9,9);
+  templates[0] = (TH1D*)h->ProjectionY("bin0",0,0);
+
   intrinsic = (TH1D*)f->Get("hLepE_sm");
   target = (TH1D*)f->Get("hElep_w");
 
-  
+  //double theta = 0.01;
+  //double dm2 = 5;
+  double par[2];
+  par[0] = 0.001;
+  par[1] = 0.5;
+
+  cout << getChi2(par) << endl;
+
+  templates[2]->Draw();
+
 }
 
 
